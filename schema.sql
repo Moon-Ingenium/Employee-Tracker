c7120a1887324612e714ab0bf4f832cea1a854dc
Michelle Moon <robbins.michelle@ymail.com>
2020-10-27 16:10:15 -0500

    fixed schema.sql

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ USE employee_trackerDB;
 CREATE TABLE role(
 id INT not null AUTO_INCREMENT,
 title VARCHAR(30) not null,
-salary DECIMAL(2,3) null,
+salary DECIMAL(10,2) null,
 department_id  int null,
 PRIMARY KEY(id)
   );
@@ -17,7 +17,7 @@ id INT not null AUTO_INCREMENT,
 first_name VARCHAR(30) not null,
 last_name VARCHAR(30) not null,
 role_id  int not null,
-manager_id INT null
+manager_id INT null,
 PRIMARY KEY(id)
   );
 
@@ -27,18 +27,17 @@ name VARCHAR(30) not null,
 PRIMARY KEY(id)
   );
 
-SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, roles.salary, CONCAT(mng.first_name," ", mng.last_name) AS manager
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(mng.first_name," ", mng.last_name) AS manager
 FROM employee
-INNER JOIN role ON empployee.roles_id = roles.id
+INNER JOIN role ON empployee.role_id = role.id
 INNER JOIN department ON role.department_id = department.id
-LEFT JOIN employee AS mng ON employee.manager_id = mng.id
-
+LEFT JOIN employee AS mng ON employee.manager_id = mng.id;
 INSERT INTO department (name)
-VALUES (Software Engineer"), ("Salesperson"), ("Lead Engineer"), ("Sales lead"), ("Accountant");
+VALUES ("Engineering"), ("Salesperson"), ("Accounting");
+
+
+
+INSERT into role (title, department_id)
+values("Software Engineer",1), ("Salesperson",2), ("Lead Engineer",1), ("Sales lead", 2), ("Accountant",3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id )
-VALUES ("Michelle", "Moon", "Engineer", "Eric Scott"), ("Sarah", "Kyle", "Salesperson", "Andrew Moon"), ("Doug", "Diaz", "Sales Lead", "Carlos Costos")("Kyle", "Kluck", "Lead Engineer", "Eric Scott"), ("Mike", "George", "Accountant", "Andrew Moon");
-
-
-INSERT into role (title, id, department_id)
-values("Software Engineer"), ("Salesperson"), ("Lead Engineer"), ("Sales lead"), ("Accountant");
-
+VALUES ("Michelle", "Moon", 1, 0), ("Sarah", "Kyle", 2, 1), ("Doug", "Diaz", 4, 2),("Kyle", "Kluck", 3, 3), ("Mike", "George", 5, 0);
